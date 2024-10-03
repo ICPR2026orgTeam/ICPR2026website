32532028569c21ec792d91e57c80c032216457aa
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2024-10-03 13:34:13 +0200

    udpate footer

--- footer.js
+++ footer.js
@@ -7,8 +7,8 @@ if (!pagename || 0 === pagename.length|| pagename=="tc18.org" || pagename=="www.
 document.getElementById("footerSite").innerHTML = '  \
 <hr /> \
 <div class="footer"> \
-<p><a href="index.html">IAPR-TC18 webpage</a> \
-You can contribute to the edition through the <a href="https://github.com/TC-18/tc18.org">Github source page</a> and directly edit this page <a href="https://github.com/TC-18/tc18.org/blob/master/'+pagename+'">here</a>. </p>\
+<p><a href="index.html">ICPR 2026 webpage</a> \
+You can contribute to the edition through the <a href="https://github.com/ICPR2026orgTeam/ICPR2026website">Github source page</a> and directly edit this page <a href="https://github.com/ICPR2026orgTeam/ICPR2026website/blob/main/'+pagename+'">here</a>. </p>\
 </div> \
 ';
 
