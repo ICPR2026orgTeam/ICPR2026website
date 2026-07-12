d05ade04f76469c3dd76ec9b6eb5d52f76db2f45
KevinHOARAU974 <kevin.hoarau@ens-paris-saclay.fr>
2026-07-12 22:40:26 +0200

    Gestion of notifications in service_worker

--- service-worker.js
+++ service-worker.js
@@ -4,4 +4,51 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
     console.log("Service worker activated");
-})
+});
+
+//Push Notification when one is received
+self.addEventListener("push", event => {
+    console.log("[Service Worker] Push received.");
+
+    let data ={
+        title: "ICPR 2026",
+        body: "New conference notification.",
+        icon: "/Logos/icprIcon-square-48.png",
+        url: "/"
+    };
+
+    if (event.data) {
+        try{
+            data = {
+                ...data,
+                ...event.data.json()
+            };
+        } catch (error) {
+            console.warn(
+                "[Service Worker] Push payload is ot JSON, using texte instead."
+            );
+
+            data.body= event.data.text();
+        }
+    }
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, {
+            body: data.body,
+            icon: data.icon,
+            data: {
+                url: data.url
+            }
+        })
+    );
+});
+
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+
+    const targetUrl = event.notification.data?.url || "/";
+
+event.waitUntil(
+    clients.openWindow(targetUrl) 
+    );
+});
